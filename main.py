36f73ed65c85a1f83a0eba5fa534940c434f94ac
Miks Matiss Liepins <skaios@inbox.lv>
2023-02-17 15:34:43 +0200

    pabeigta versija

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ def find_mismatch(text):
                 return i+1    
             opening_brackets_stack.pop()
 
-        if opening_brackets_stack:
-            return opening_brackets_stack[-1].position
-        return "Success"
+    if opening_brackets_stack:
+        return opening_brackets_stack[-1].position
+    return "Success"
 
 
 
