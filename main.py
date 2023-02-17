d222e6e686deba82d61397a034516912b385ed6d
Miks Matiss Liepins <skaios@inbox.lv>
2023-02-17 15:23:25 +0200

    version 1

--- main.py
+++ main.py
@@ -14,17 +14,25 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next, i + 1))
             pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+                return i+1    
+            opening_brackets_stack.pop()
+
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    if mismatch:
+        return mismatch[-1].position
+    return "Success"   
+    print(mismatch)
 
 
 if __name__ == "__main__":
